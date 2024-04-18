ceddc70d1432bbb27eb501d44aae0aaeef252455
FransHessel <fw.hesseling@student.han.nl>
2024-04-18 14:43:57 +0200

    Schieten  laser met Space werkend

--- src/main/java/AlienDestruction/Scenes/GameScreen.java
+++ src/main/java/AlienDestruction/Scenes/GameScreen.java
@@ -9,15 +9,23 @@ import AlienDestruction.Entities.MenuBar.BlackRectangle;
 import AlienDestruction.Entities.MenuBar.PlayerLivesText;
 import AlienDestruction.Entities.MenuBar.PlayerLivesSprite;
 import AlienDestruction.Entities.Player;
+import AlienDestruction.Weapons.IShootable;
+import AlienDestruction.Weapons.LaserGun;
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.EntitySpawnerContainer;
 import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.EntitySpawner;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 
 
-public class GameScreen extends DynamicScene {
+public class GameScreen extends DynamicScene implements EntitySpawnerContainer {
+
     protected App app;
+    private Player player;
+    private final EntitySpawner weaponTypeSpawner;
     public GameScreen(App app) {
         this.app = app;
+        this.weaponTypeSpawner = new LaserGun(10);
     }
 
     @Override
@@ -25,18 +33,19 @@ public class GameScreen extends DynamicScene {
         setBackgroundImage("backgrounds/universe2.jpg");
     }
 
-
     @Override
     public void setupEntities() {
-        var enemySpriteOne = new EnemyOne(new Coordinate2D(40, -40));
+        player = new Player(new PlayerLivesText(new Coordinate2D(60, 20)),new Coordinate2D(getWidth() / 2, 550));
+        player.setWeapon((IShootable) weaponTypeSpawner);
 
+        var enemySpriteOne = new EnemyOne(new Coordinate2D(40, -40));
         addEntity(enemySpriteOne);
 
         var enemySpriteTwo = new EnemyTwo(new Coordinate2D(140, -40));
         addEntity(enemySpriteTwo);
 
         var playerLivesText = new PlayerLivesText(new Coordinate2D(60, 20));
-        var player = new Player(playerLivesText, new Coordinate2D(getWidth() / 2, 550));
+        //var player = new Player(playerLivesText, new Coordinate2D(getWidth() / 2, 550));
 
         addEntity(player);
 
@@ -54,5 +63,14 @@ public class GameScreen extends DynamicScene {
 
         MenuButton menuButton = new MenuButton(app, new Coordinate2D(930, 20));
         addEntity(menuButton);
+    }
+
+    @Override
+    public void setupEntitySpawners() {
+        addEntitySpawner(weaponTypeSpawner);
+
+        if (weaponTypeSpawner.isActive()) {
+            weaponTypeSpawner.pause();
+        }
     }
 }
