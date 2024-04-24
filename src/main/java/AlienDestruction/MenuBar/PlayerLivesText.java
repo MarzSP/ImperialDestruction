be2d95612afc893b3bd8239e775e4fce1e81af16
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-04-24 21:58:33 +0200

    PlayerLivesText (getLives())

--- src/main/java/AlienDestruction/MenuBar/PlayerLivesText.java
+++ src/main/java/AlienDestruction/MenuBar/PlayerLivesText.java
@@ -1,6 +1,7 @@
 package AlienDestruction.MenuBar;
 
 import AlienDestruction.App;
+import AlienDestruction.Entities.Player;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.impl.TextEntity;
 import javafx.scene.paint.Color;
@@ -9,12 +10,18 @@ import javafx.scene.text.FontWeight;
 
 public class PlayerLivesText extends TextEntity {
 
+    private Player player;
 
-    public PlayerLivesText(Coordinate2D initialLocation) {
+    public PlayerLivesText(Coordinate2D initialLocation, Player player) {
         super(initialLocation);
+        this.player = player;
 
         setFont(Font.font("Roboto", FontWeight.NORMAL, 30));
         setFill(Color.ANTIQUEWHITE);
+        updateText(); // Update text initially
     }
 
-}
+    private void updateText() {
+        setText("Lives: " + player.getLives());
+    }
+    }
