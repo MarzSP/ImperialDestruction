56a6cce19e28a2c34469965cb8370a8194be4070
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-04-30 11:16:42 +0200

    Documentatie: Classes en methodes in Package PowerUps. Maar NIET PowerUpShip.

--- src/main/java/AlienDestruction/Entities/PowerUps/PowerUpLives.java
+++ src/main/java/AlienDestruction/Entities/PowerUps/PowerUpLives.java
@@ -3,16 +3,22 @@ package AlienDestruction.Entities.PowerUps;
 import AlienDestruction.Helper;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.Size;
-import com.github.hanyaeger.api.entities.Collider;
 import com.github.hanyaeger.api.entities.ContinuousRotatable;
-import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 
-/**De klasse PowerUpLives is een power-up in de game die 1 extra leven toevoegt aan de speler.
+/**De klasse PowerUpLives is een power-up in de game die 1 extra leven toevoegd aan de speler.
  * Deze klasse erft van de PowerUps klasse en implementeert de ContinuousRotatable interface.
  * De implementatie van PowerUpLives zit in Player in OnCollision.
  */
 public class PowerUpLives extends PowerUps implements ContinuousRotatable {
 
+    /**
+     * Constructor:
+     * @param initialLocation : De beginpositie van de power-up op het scherm (als Coordinate2D object)
+     * Sprite afbeelding: "sprites/xWingPowerUp.png" : een visuele representatie van een extra leven
+     * Grootte: Size(40,40)
+     * Hoge beginsnelheid en neerwaartse richting (met behulp van Helper.Speed.HIGH en Helper.Direction.DOWN)
+     * Rotatiesnelheid: 2
+     */
     public PowerUpLives (Coordinate2D initialLocation) {
         super("sprites/xWingPowerUp.png", initialLocation, new Size(40,40));
         setMotion(Helper.Speed.HIGH, Helper.Direction.DOWN);
